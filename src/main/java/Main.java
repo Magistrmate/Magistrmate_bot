199a5a077ea829e6d20db6c8099b60fd9f3cf1de
Magistrmate <retrok@yandex.ru>
2022-05-28 18:06:36 +0300

    Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
--- src/main/java/MyAmazingBot.java
+++ src/main/java/MyAmazingBot.java
@@ -1,9 +1,19 @@
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MyAmazingBot extends TelegramLongPollingBot {
+    private static final Logger log = Logger.getLogger(Main.class);
+
     @Override
     public String getBotUsername() {
         return "Magistrmate_bot";
@@ -16,17 +26,45 @@ public class MyAmazingBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getFrom().getFirstName());
-            //message.setText("Hi" + update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        log.debug("Receive new Update. updateID: " + update.getUpdateId());
+        String inputText = update.getMessage().getText();
+
+        Message message = update.getMessage();
+        if (inputText.startsWith("/start")) {
+            sendMsg(message, "Добро пожаловать " + update.getMessage().getFrom().getFirstName() + "!");
+        } else if (update.hasMessage() && update.getMessage().hasText()) {
+            if (update.getMessage().getText().toLowerCase(Locale.ROOT).contains("привет")) {
+                sendMsg(message, "Дороу");
+            } else {
+                sendMsg(message, "Даже не знаю, что на это ответить");
             }
+        }
+    }
+    private void sendMsg (Message message, String text) {
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRow = new ArrayList<>();
+        KeyboardRow keyboard = new KeyboardRow();
+        keyboard.add("Name");
+        keyboard.add("Add");
+        keyboardRow.add(keyboard);
+        replyKeyboardMarkup.setKeyboard(keyboardRow);
+
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(sendMessage.getReplyToMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        }  catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 }
