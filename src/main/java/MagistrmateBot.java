f62166274541414f8414b18150ec39de7db89b55
Magistrmate <retrok@yandex.ru>
2022-05-31 14:51:37 +0300

    Version5

--- src/main/java/MagistrmateBot.java
+++ src/main/java/MagistrmateBot.java
@@ -16,6 +16,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.*;
 
 public class MagistrmateBot extends TelegramLongPollingBot {
+    public static final String PZV_PICTURE = "AgACAgIAAxkBAAIBx2KTdP4CNbqTZfv7Hm7TqGAugkdSAAKIvjEbUaqZSPwL-Up482owAQADAgADeQADJAQ";
+    public static final String POD_PICTURE = "AgACAgIAAxkBAAICIWKV6rrWZCfAVHeZRb600fEdhUUtAALFuTEbgvKxSGOQfTzAxJOgAQADAgADeQADJAQ";
+    public static final String KORR_PICTURE = "AgACAgIAAxkBAAICHWKV6h4KSQAB0Cw-9YYzNmKqQYlBbAACwrkxG4LysUioRIR5O_3J4wEAAwIAA3kAAyQE";
+    public static final String LUNN_PICTURE = "AgACAgIAAxkBAAICH2KV6lrfNbsYfnqVDQwV1uhexKUGAALDuTEbgvKxSBEP75XimlVwAQADAgADeQADJAQ";
+    public static final String ZVEZDA_PICTURE = "AgACAgIAAxkBAAICG2KV6b_hGJ0jk_yHtzUWmfXrs0K-AALBuTEbgvKxSE55vwxxNxcxAQADAgADeQADJAQ";
+    public static final String PON_PICTURE = "AgACAgIAAxkBAAICI2KV6u5ALDMcSPP4WPsvdr5iBJ1hAALGuTEbgvKxSGlGhmGbA1qtAQADAgADeQADJAQ";
+
     private static final Logger log = Logger.getLogger(Main.class);
 
     @Override
@@ -36,14 +43,25 @@ public class MagistrmateBot extends TelegramLongPollingBot {
             String inputText = update.getMessage().getText();
             if (inputText.equals("/start")) {
                 sendMsg(message, "Добро пожаловать " + update.getMessage().getFrom().getFirstName() + "!");
-                //sndPhoto(message);
                 List<InputMedia> media = new ArrayList<>();
                 InputMedia photo1 = new InputMediaPhoto();
-                photo1.setMedia("AgACAgIAAxkBAAIBx2KTdP4CNbqTZfv7Hm7TqGAugkdSAAKIvjEbUaqZSPwL-Up482owAQADAgADeQADJAQ");
+                photo1.setMedia(PZV_PICTURE);
                 InputMedia photo2 = new InputMediaPhoto();
-                photo2.setMedia("AgACAgIAAxkBAAIBzWKTeujFc3jBbsjGDvAz-mRzBjagAALiuTEb5HCYSCsiIM87y116AQADAgADeQADJAQ");
+                photo2.setMedia(POD_PICTURE);
+                InputMedia photo3 = new InputMediaPhoto();
+                photo3.setMedia(KORR_PICTURE);
+                InputMedia photo4 = new InputMediaPhoto();
+                photo4.setMedia(LUNN_PICTURE);
+                InputMedia photo5 = new InputMediaPhoto();
+                photo5.setMedia(ZVEZDA_PICTURE);
+                InputMedia photo6 = new InputMediaPhoto();
+                photo6.setMedia(PON_PICTURE);
                 media.add(photo1);
                 media.add(photo2);
+                media.add(photo3);
+                media.add(photo4);
+                media.add(photo5);
+                media.add(photo6);
                 SendMediaGroup mediaGroup = new SendMediaGroup();
                 mediaGroup.setChatId(message.getChatId().toString());
                 mediaGroup.setMedias(media);
@@ -52,55 +70,41 @@ public class MagistrmateBot extends TelegramLongPollingBot {
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
-
             } else if (update.getMessage().getText().toLowerCase(Locale.ROOT).contains("привет")) {
                 sendMsg(message, "Дороу");
             } else
                 sendMsg(message, "Даже не знаю, что на это ответить");
         } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
-                // Message contains photo
-                // Set variables
                 long chat_id = update.getMessage().getChatId();
-
-                // Array with photo objects with different sizes
-                // We will get the biggest photo from that array
                 List<PhotoSize> photos = update.getMessage().getPhoto();
-                // Know file_id
                 String f_id = Objects.requireNonNull(photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
                         .orElse(null)).getFileId();
-                // Set photo caption
                 String caption = "file_id: " + f_id;
                 SendPhoto msg = new SendPhoto();
                 msg.setPhoto(new InputFile(f_id));
                 msg.setChatId(String.valueOf(chat_id));
                 msg.setCaption(caption);
-
                 try {
-                    execute(msg); // Call method to send the photo with caption
+                    execute(msg);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
         }
     }
     private void sendMsg (Message message, String text) {
-
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
-
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(false);
         sendMessage.setReplyMarkup(replyKeyboardMarkup);
-
         List<KeyboardRow> keyboardRow = new ArrayList<>();
         KeyboardRow keyboard = new KeyboardRow();
         keyboard.add("Читать фрагмент");
         keyboard.add("Книги");
-
         keyboardRow.add(keyboard);
         replyKeyboardMarkup.setKeyboard(keyboardRow);
-
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(sendMessage.getReplyToMessageId());
         sendMessage.setText(text);
