a20a2b64c03d438a2c1f252d8608763dd7b437a8
Magistrmate <101048039+Magistrmate@users.noreply.github.com>
2022-10-05 16:00:24 +0300

    Update BotConfig.java

--- src/main/java/BotConfig.java
+++ src/main/java/BotConfig.java
@@ -1,5 +1,5 @@
 public class BotConfig {
-    public static final String BOT_TOKEN = "5209127731:AAGNyiwXA-icW7KXwxKmLo94jq3wo-czbSE";
-    public static final String DB_TOKEN = "mongodb+srv://Magistrmate:6RqUMRQCxGPZn2qe@magistrmatedatabase.amnzkaf.mongodb.net/?retryWrites=true&w=majority";
-    public static final String ID_SUPPORT = "5791523535";
+    public static final String BOT_TOKEN = "";
+    public static final String DB_TOKEN = "";
+    public static final String ID_SUPPORT = "";
 }
